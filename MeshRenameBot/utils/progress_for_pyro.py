211741c9ff2fd88ab5db5c55eba4dd430a85bbc8
nitanuj <103054544+nitanuj@users.noreply.github.com>
2022-06-29 17:47:58 +0530

    Update progress_for_pyro.py

--- MeshRenameBot/utils/progress_for_pyro.py
+++ MeshRenameBot/utils/progress_for_pyro.py
@@ -43,12 +43,12 @@ async def progress_for_pyrogram(
         elapsed_time = human_readable_timedelta(elapsed_time)
         estimated_total_time = human_readable_timedelta(estimated_total_time)
 
-        progress = "[{0}{1}] \nP: {2}%\n".format(
-            ''.join([get_var("COMPLETED_STR") for _ in range(math.floor(percentage / 10))]),
-            ''.join([get_var("REMAINING_STR") for _ in range(10 - math.floor(percentage / 10))]),
+        progress = "[{0}{1}] \n\n🔋P: {2}%\n\n📥📤".format(
+            ''.join([get_var("COMPLETED_STR") for _ in range(math.floor(percentage / 5))]),
+            ''.join([get_var("REMAINING_STR") for _ in range(10 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+        tmp = progress + "{0} of {1}\n\n🚀Speed: {2}/s\n\n⏳ETA: {3}\n".format(
             human_readable_bytes(current),
             human_readable_bytes(total),
             human_readable_bytes(speed),
