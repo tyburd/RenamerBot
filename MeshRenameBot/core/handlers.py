2de8420ea6a7e1954b15d801b3d6e69881eef25b
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2022-07-11 08:45:29 +0530

    Update handlers.py

--- MeshRenameBot/core/handlers.py
+++ MeshRenameBot/core/handlers.py
@@ -48,7 +48,7 @@ def add_handlers(client: Client) -> None:
 
 
 async def start_handler(client: Client, msg: Message) -> None:
-    await msg.reply(Trans.START_MSG, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('⋆ Support ⋆', url=f"https://t.me/isabella_support"),InlineKeyboardButton('⋆ 𝙾𝚠𝚗𝚎𝚛 ⋆', url=f"tg://user")]]))
+    await msg.reply(Trans.START_MSG, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('⋆ Support ⋆', url=f"https://t.me/isabella_support"),InlineKeyboardButton('⋆ 𝙷𝚎𝚕𝚙 ⋆', callback_data="help_str")]]))
         
 
 
