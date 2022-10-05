880587c16879509cc7d36002744a071b60476f10
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-10-05 22:39:36 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -17,7 +17,7 @@ except ImportError:
         # Access Restriction
         IS_PRIVATE = [bool, False]
         AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 123456789]
+        OWNER_ID = "5591954930"
 
         # Public username url or invite link of private chat
         FORCEJOIN = [str,""]
