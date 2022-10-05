16a042c215be3fdb6611870f64db0d1037b297f3
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-10-05 22:55:36 +0530

    Update __main__.py

--- MeshRenameBot/__main__.py
+++ MeshRenameBot/__main__.py
@@ -2,7 +2,7 @@ from .core.get_config import get_var
 from .core.handlers import add_handlers
 from .mesh_bot import MeshRenameBot
 from . maneuvers.ExecutorManager import ExecutorManager
-from loader import load_plugins
+from MeshRenameBot.loader import load_plugins
 import logging
 
 logging.basicConfig(
