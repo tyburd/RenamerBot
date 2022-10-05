22a6f6e1ba12a8e9d5459fdc91f2236eacb4d4ac
Yi <114733973+YiZaha@users.noreply.github.com>
2022-10-05 22:50:31 +0530

    Update __main__.py

--- MeshRenameBot/__main__.py
+++ MeshRenameBot/__main__.py
@@ -2,6 +2,7 @@ from .core.get_config import get_var
 from .core.handlers import add_handlers
 from .mesh_bot import MeshRenameBot
 from . maneuvers.ExecutorManager import ExecutorManager
+from loader import load_plugins
 import logging
 
 logging.basicConfig(
@@ -11,6 +12,8 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 # TODO Add a alert for an extra space recorded
+
+load_plugins("broadcast") # just lazy stuff
 
 if __name__ == "__main__":
 
